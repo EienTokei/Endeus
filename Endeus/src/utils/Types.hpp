7f4cc2a02f2897d24ddeac1315fabf19e16022fc
Eien Tokei <himenosena1165@gmail.com>
2026-05-22 21:49:39 +0800

    feat(types): add == and != for Vec2f and Rect

--- Endeus/src/utils/Types.hpp
+++ Endeus/src/utils/Types.hpp
@@ -5,6 +5,14 @@ namespace endeus {
 	struct Vec2f {
 		float x = 0.f;
 		float y = 0.f;
+
+		bool operator==(const Vec2f& other) const {
+			return this->x == other.x && this->y == other.y;
+		}
+
+		bool operator!=(const Vec2f& other) const {
+			return !(*this == other);
+		}
 	};
 
 	struct Rect {
@@ -12,6 +20,15 @@ namespace endeus {
 		int y = 0;
 		int width = 0;
 		int height = 0;
+
+		bool operator==(const Rect& other) const {
+			return this->x == other.x && this->y == other.y
+				&& this->width == other.width && this->height == other.height;
+		}
+
+		bool operator!=(const Rect& other) const {
+			return !(*this == other);
+		}
 	};
 
 } // namespace endeus
