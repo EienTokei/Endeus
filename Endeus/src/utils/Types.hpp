409e177863aee95ca910dfe2ee33b5d9d36cd962
Eien Tokei <himenosena1165@gmail.com>
2026-05-25 12:47:10 +0800

    feat(utils): 为 Vec2f 添加常用运算符重载
    - 支持 ==, !=, +, -, *, +=, -=, *=
    - 支持标量乘法交换律
    - 使用 inline 避免多重定义

--- Endeus/src/utils/Types.hpp
+++ Endeus/src/utils/Types.hpp
@@ -6,14 +6,39 @@ namespace endeus {
 		float x = 0.f;
 		float y = 0.f;
 
-		bool operator==(const Vec2f& other) const {
-			return this->x == other.x && this->y == other.y;
+		// 非成员函数保证左右操作数的对称性和隐式转换的一致性
+		friend bool operator==(const Vec2f& a, const Vec2f& b) {
+			return a.x == b.x && a.y == b.y; 
 		}
 
-		bool operator!=(const Vec2f& other) const {
-			return !(*this == other);
+		friend bool operator!=(const Vec2f& a, const Vec2f& b) {
+			return !(a == b);
+		}
+
+		Vec2f& operator+=(const Vec2f& other) {
+			x += other.x;
+			y += other.y;
+			return *this;
+		}
+
+		Vec2f& operator-=(const Vec2f& other) {
+			x -= other.x;
+			y -= other.y;
+			return *this;
+		}
+
+		Vec2f& operator*=(float scalar) {
+			x *= scalar;
+			y *= scalar;
+			return *this;
 		}
 	};
+
+	// 对于外部非成员运算符重载, 没有标记 inline 的话每个包含它的 .cpp 都会生成一份函数体，导致多重定义
+	inline Vec2f operator+(Vec2f a, const Vec2f& b) { a += b; return a; }
+	inline Vec2f operator-(Vec2f a, const Vec2f& b) { a -= b; return a; }
+	inline Vec2f operator*(Vec2f v, float scalar) { v *= scalar; return v; }
+	inline Vec2f operator*(float scalar, Vec2f v) { v *= scalar; return v; }
 
 	struct Rect {
 		int x = 0;
